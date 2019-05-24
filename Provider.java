f8cc5ca04ce4d097bc8e15a6dc30a388212ff7d6
qing <qingzgh@gmail.com>
2019-05-24 11:50:27 +0800

    Provider version1.0

---
+++ Provider.java
@@ -0,0 +1,23 @@
+/*
+
+package com.lianjia.DubboDemo.DubboDemoProvider;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class Provider {
+
+    public static void main(String[] args) throws IOException {
+
+// tomcat启动这个 初始化spring环境 并接受
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[]{"dubbo-demo-provider.xml"});
+
+        ac.start();
+        System.in.read();
+
+    }
+}
+*/
+
